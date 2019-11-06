54f2833d38c103903b5d30dadf5cf07462c0b77e
Maxim <coldglock17@gmail.com>
2019-11-06 16:56:19 +0700

    Final v2

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,21 @@
-var cartLink = document.querySelector(".buy-button");
+var cartLink = document.querySelectorAll(".buy-button");
 var cartPopup = document.querySelector(".modal-cart");
 var cartClose = cartPopup.querySelector(".cart-close");
-
-cartLink.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	cartPopup.classList.remove("visually-hidden");
-});
+var cartContinue = cartPopup.querySelector(".modal-continue");
 
 cartClose.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	cartPopup.classList.add("visually-hidden");
 });
+
+cartContinue.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	cartPopup.classList.add("visually-hidden");
+});
+
+for (var i = 0; i < cartLink.length; i++ ) {
+	cartLink[i].addEventListener("click", function (evt) {
+	evt.preventDefault();
+	cartPopup.classList.remove("visually-hidden");
+});
+}
